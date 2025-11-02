ca6a9f94d7888ad1483b87df340f4b76855e1c24
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-11-02 17:59:06 +0300

    Вынесена логика валидации фильмов из контроллера в сервис

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,15 +3,12 @@ package ru.yandex.practicum.filmorate.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
@@ -21,31 +18,27 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FilmController {
 
-    @Autowired
-    private FilmStorage filmStorage;
     private final FilmService filmService;
 
     @GetMapping
     public Collection<Film> getAll() {
-        return filmStorage.getFilms();
+        return filmService.getAllFilms();
     }
 
     @GetMapping("/{filmId}")
     public Film getFilm(@PathVariable int filmId) {
-        return filmStorage.getFilmById(filmId);
+        return filmService.getFilmById(filmId);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Film create(@Valid @RequestBody Film film) {
-        validateFilmReleaseDate(film);
-        return filmStorage.create(film);
+        return filmService.createFilm(film);
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
-        validateFilmReleaseDate(film);
-        return filmStorage.update(film);
+        return filmService.updateFilm(film);
     }
 
     @PutMapping("/{filmId}/like/{userId}")
@@ -69,14 +62,5 @@ public class FilmController {
     public List<Film> getPopular(@RequestParam(defaultValue = "10") int count) {
         if (count <= 0) throw new ValidationException("Размер выборки популярных фильмов должен быть положительным");
         return filmService.getMostLikedFilms(count);
-    }
-
-    private void validateFilmReleaseDate(Film film) {
-        LocalDate filmReleaseDate = film.getReleaseDate();
-        if (filmReleaseDate.isBefore(LocalDate.of(1895, 12, 28))) {
-            log.error("Дата выхода фильма ({}) указана до дня рождения кино (28 декабря 1895)", filmReleaseDate);
-            throw new ValidationException("Дата выхода фильма (" + filmReleaseDate + ") " +
-                    "указана до дня рождения кино (28 декабря 1895)");
-        }
     }
 }
