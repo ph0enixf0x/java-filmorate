ef4ec216923111d95a8bd84f45cf8f42caef9383
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-11-02 13:29:04 +0300

    Добавлены интерфейсы слоя хранилищ

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+public interface FilmStorage {
+    public void create(Film film);
+
+    public void update(Film film);
+
+    public void delete(int id);
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+public interface UserStorage {
+    public void create(User user);
+
+    public void update(User user);
+
+    public void delete(int id);
+}
