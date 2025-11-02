b4a5e2edf0307c68717e7cabdd78c4c634e9be95
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-11-02 13:29:18 +0300

    Добавлена реализация интерфейса хранилища фильмов

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Integer, Film> films = new HashMap<>();
+
+
+    @Override
+    public void create(Film film) {
+        films.put(film.getId(), film);
+    }
+
+    @Override
+    public void update(Film film) {
+        int filmId = film.getId();
+        if (films.containsKey(filmId)) {
+            films.put(filmId, film);
+            //log.info("Обновлен фильм: {}", newFilm);
+        } else {
+            //log.error("Фильм с идентификатором {} не найден!", filmId);
+            throw new NotFoundException("Фильм с идентификатором " + filmId + " не найден!");
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        films.remove(id);
+    }
+}
