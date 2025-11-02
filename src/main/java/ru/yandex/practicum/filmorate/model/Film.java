b2bbc2058c7eaacc0763550d3e6370b09f73c095
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-11-02 17:50:15 +0300

    Добавлена валидация поля даты релиза фильма

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -16,6 +17,7 @@ public class Film {
     private String name;
     @Size(max = 200)
     private String description;
+    @NotNull
     private LocalDate releaseDate;
     @Positive
     private int duration;
