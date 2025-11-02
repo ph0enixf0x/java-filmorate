9b49f906f5b1dccfe2bd9df6b22216ec830e8dfd
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-11-02 14:40:05 +0300

    Добавлен сервис обработки бизнес логики пользователей

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 public class User {  //TODO: Переписать валидацию на Spring Validator
@@ -11,4 +12,5 @@ public class User {  //TODO: Переписать валидацию на Spring
     private String login;
     private String name;
     private LocalDate birthday;
+    private Set<Integer> friends;
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserStorage userStorage;
+
+    public void addFriend(int userId, int friendId) {
+        User originalUser = userStorage.getUserById(userId);
+        User friendUser = userStorage.getUserById(friendId);
+        originalUser.getFriends().add(friendId);
+        friendUser.getFriends().add(userId);
+    }
+
+    public void removeFriend(int userId, int friendId) {
+        User originalUser = userStorage.getUserById(userId);
+        User friendUser = userStorage.getUserById(friendId);
+        originalUser.getFriends().remove(friendId);
+        friendUser.getFriends().remove(userId);
+    }
+
+    public List<User> getFriends(int userId) {
+        return userStorage.getUserById(userId).getFriends().stream()
+                .map(friendId -> userStorage.getUserById(friendId))
+                .toList();
+    }
+}
